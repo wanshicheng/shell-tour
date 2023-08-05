c8f3e4f3b077c617668e53ae354e9b36b6cb5bda
wanshicheng <wanshicheng@qq.com>
2023-08-05 15:47:23 +0800

    Create init.sh

---
+++ raspberry-pi/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo bash -c 'cat > /etc/apt/sources.list <<EOF
+deb https://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb https://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb-src https://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb https://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb https://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+EOF
+'
+
+
+sudo apt-get update 
+sudo apt-get upgrade -y
