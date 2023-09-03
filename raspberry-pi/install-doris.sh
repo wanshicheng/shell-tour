93ec818588912c1eed03397d41d3770fdb3d5bcb
wanshicheng <wanshicheng@qq.com>
2023-09-03 11:47:21 +0800

    Update install-doris.sh

--- raspberry-pi/install-doris.sh
+++ raspberry-pi/install-doris.sh
@@ -21,11 +21,6 @@ sudo dphys-swapfile swapoff
 sudo service dphys-swapfile stop
 sudo systemctl disable dphys-swapfile
 
-echo "开始安装并配置JDK..."
-tar xf ./jdk-8u381-linux-aarch64.tar.gz
-mv ./jdk1.8.0_381 ./jdk
-
-
 sudo sysctl -w vm.max_map_count=2000000
 sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
 sudo bash -c 'cat >> /etc/sysctl.conf <<EOF
@@ -33,12 +28,19 @@ vm.max_map_count=2000000
 EOF
 '
 
+echo "开始安装并配置JDK..."
+tar xf ./jdk-8u381-linux-aarch64.tar.gz
+mv ./jdk1.8.0_381 ./jdk
+
 sudo bash -c 'cat > /etc/profile.d/jdk.sh <<EOF
 export JAVA_HOME=/opt/jdk
 export JRE_HOME=/opt/jdk/jre
 export PATH=\$JAVA_HOME/bin:\$CATALINA_HOME/bin:\$JRE_HOME:\$PATH
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 EOF'
+
+
+echo "请执行source source /etc/profile.d/jdk.sh"
 
 echo "开始安装Doris..."
 tar xf ./apache-doris-1.2.6-bin-arm64.tar.xz
